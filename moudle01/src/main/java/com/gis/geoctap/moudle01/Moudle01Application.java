11c25a267a0025cff1548cb3959c32ff1b27998e
Binwen Wu <2020302131043@whu.edu.cn>
2023-04-25 15:18:47 +0800

    项目初始化完成

---
+++ moudle01/src/main/java/com/gis/geoctap/moudle01/Moudle01Application.java
@@ -0,0 +1,13 @@
+package com.gis.geoctap.moudle01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Moudle01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Moudle01Application.class, args);
+    }
+
+}
---
+++ moudle01/src/test/java/com/gis/geoctap/moudle01/Moudle01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gis.geoctap.moudle01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Moudle01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
